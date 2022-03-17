6f47e40b129fc70398d3c669597e757e7994c38d
JithinPeterKasper <jithin@techis.io>
2022-03-17 14:51:35 +0530

    Added static files

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -141,9 +141,7 @@ cloudinary.config(
     api_secret="UOiBaa-4DvHkzo9VZFzIv156tW4",
 )
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
---
+++ staticfiles/js/posts.js
@@ -0,0 +1,6 @@
+/////////Javascript////////
+$(function(){
+    $('.js-menu-icon').click(function(){
+        $(this).next().toggle();
+    })
+})
